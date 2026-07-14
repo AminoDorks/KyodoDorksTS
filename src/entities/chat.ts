97c92d1062c5a3f02dcd59b9b9e963b546439cbf
thatcelt <147246163+thatcelt@users.noreply.github.com>
2026-07-14 20:44:56 +0300

    fix(entities/chat): fix missing optional field

--- src/entities/chat.ts
+++ src/entities/chat.ts
@@ -5,19 +5,19 @@ import { ShortMemberSchema } from './member';
 export const ChatSchema = z.object({
   id: z.string(),
   circleId: z.string(),
-  icon: z.string(),
-  name: z.string(),
+  icon: z.string().optional(),
+  name: z.string().optional(),
   type: z.number(),
   status: z.number(),
   memberCount: z.number(),
   memberSummary: z.array(ShortMemberSchema),
   lastActiveTime: z.string(),
-  background: z.string().nullable(),
-  createdTime: z.string(),
-  modifiedTime: z.string(),
+  background: z.string().nullable().optional(),
+  createdTime: z.string().optional(),
+  modifiedTime: z.string().optional(),
   coHostUids: z.array(z.string()),
   isReadOnly: z.boolean(),
-  content: z.string().nullable(),
+  content: z.string().nullable().optional(),
 });
 
 export type Chat = z.infer<typeof ChatSchema>;
